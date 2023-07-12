a05661c68846eb412c4332cd8426d1f11e9683fa
Pranjal Goyal <pranjalg984@gmail.com>
2023-07-12 11:35:43 +0530

    add server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express =require ('express');
 const app=express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.json({online:"coompiler"})
+});
+app.post("/run",(req,res)=>{
+    const language=req.body.language;
+    const code =req.body.code;
+    res.json({language,body});
 });
 app.listen(8000,()=>{
     console.log("Server is listening on port 8000");
